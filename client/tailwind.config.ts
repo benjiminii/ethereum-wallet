9536bf1028d470ed4902655657caf71d4180bad5
benjiminii <bilguunaiax7@gmail.com>
2024-01-11 10:00:26 +0800

    feat: added UI libraries to client

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: {},
+      colors: {
+        main1: "#080808",
+        main2: "#131313",
+        neon: "#68ff5b",
+        pink1: "#d36492",
+        green1: "#5ada86",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("daisyui")],
+};
